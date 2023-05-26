cbb8dfa3acaedfc92dd51abd1064f3067fdf13d1
Alex Sander <alexsandermanhes@gmail.com>
2023-05-25 23:10:31 -0300

    02-04-array

--- MJS/02-04-array/script.js
+++ MJS/02-04-array/script.js
@@ -1,18 +1,19 @@
+//nao se usa idade para banco de dados.
 var person1 = {
-    name: Alex,
+    name: 'Alex',
     age: 23
 }
-var person2 ={
-    name: Keleve,
+var person2 = {
+    name: 'Keleve',
     age: 21
 }
 var person3 = {
-    name: Gustavo,
+    name: 'Gustavo',
     age: 21
 }
 
-var list = [ person1, person2, person3]
+var list = [person1, person2, person3]
 
-for( var person0 of list){
+for (var person0 of list) {
     console.log(person0)
 }
